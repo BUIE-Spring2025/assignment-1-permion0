821a3891ced0af9b3223de3e31be7e1b58058eb1
permion0 <necmi.gorgulu@std.bogazici.edu.tr>
2025-03-10 01:48:03 +0300

    for loop for appending

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def int_to_roman(num):
     :return: A string representing the Roman numeral of the integer.
     """
     roman_numbers = [(1000,"M"), (900, "CM"), (500,"D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+    result = []
+
+    for value, roman in roman_numbers:
+        count = num // value
+        if count > 0:
+            result.append(roman * count)
+            num -= value * count
+        elif num == 0:
+            break
